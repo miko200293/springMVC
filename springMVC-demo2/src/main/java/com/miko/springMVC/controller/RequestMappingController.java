bc20dc41acac5895090355bee77abf6d40e7954a
miko200293 <1048493156qwe@gmail.com>
2023-05-29 16:46:08 +0800

    HelloWorld day01-3

--- springMVC-demo2/src/main/java/com/miko/springMVC/controller/RequestMappingController.java
+++ springMVC-demo2/src/main/java/com/miko/springMVC/controller/RequestMappingController.java
@@ -1,13 +1,38 @@
 package com.miko.springMVC.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 @Controller
 //RequestMapping("/hello")
 public class RequestMappingController {
 
-    @RequestMapping(value={"/testRequestMapping","/test"})
+    @RequestMapping(
+            value={"/testRequestMapping","/test"},
+            method = {RequestMethod.GET,RequestMethod.POST}
+    )
     public String success(){
+        return "success";
+    }
+
+    @GetMapping("/testGetMapping")
+    public String testGetMapping(){
+        return "success";
+
+    }
+
+    @RequestMapping(value = "/testPut",method = RequestMethod.PUT)
+    public String testPost(){
+        return "success";
+    }
+
+    //@RequestMapping("/a?a/testAnt")
+    //@RequestMapping("/a*a/testAnt")
+    @RequestMapping("/**/testAnt")
+
+    public String testAnt(){
         return "success";
     }
 }
