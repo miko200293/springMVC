8dac35d42b90e0379b20ea491f1f273d5bc3ec19
miko200293 <1048493156qwe@gmail.com>
2023-05-31 18:23:00 +0800

    HelloWorld day2-3

--- springMVC-rest/src/main/java/com/miko/rest/controller/EmployeeController.java
+++ springMVC-rest/src/main/java/com/miko/rest/controller/EmployeeController.java
@@ -1,11 +1,32 @@
 package com.miko.rest.controller;
 
+import com.miko.rest.bean.Employee;
 import com.miko.rest.dao.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Collection;
 
 @Controller
 public class EmployeeController {
     @Autowired
     private EmployeeDao employeeDao;
+
+    @RequestMapping(value = "/employee",method = RequestMethod.GET)
+    public String getAllEmployee(Model model){
+        Collection<Employee> employeeList=employeeDao.getAll();
+        model.addAttribute("employeeList",employeeList);
+        return "employee_list";
+    }
+
+    @RequestMapping(value = "/employee/{id}",method = RequestMethod.DELETE)
+    public String deleteEmployee(@PathVariable("id") Integer id){
+        employeeDao.delete(id);
+        return "redirect:/employee";
+    }
 }
