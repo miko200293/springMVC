e1dbf298f9faedba5753f094e4812137241c7746
miko200293 <1048493156qwe@gmail.com>
2023-06-02 20:40:59 +0800

    HelloWorld day5

---
+++ springMVC-annotation/src/main/java/com/miko/mvc/interceptor/TestInterceptor.java
@@ -0,0 +1,15 @@
+package com.miko.mvc.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("TestInterceptor->preHandle");
+        return true;
+    }
+
+}
